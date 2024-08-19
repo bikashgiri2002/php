824aa24092b7317f2bd03ffca0af587ddbd08be1
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-08-19 23:18:27 +0530

    log in

--- e-commerce/index.php
+++ e-commerce/index.php
@@ -11,6 +11,36 @@
     <title>Geo Hub</title>
   </head>
   <body>
+  <header>
+      <nav>
+        <div class="nav_icon">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+            <path
+              d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"
+            />
+          </svg>
+        </div>
+        <div class="options">
+          <ul>
+            <li>Home</li>
+            <li>
+              <select>
+                <option >Catagories</option>
+                <option >Hammer</option>
+                <option >Compus</option>
+                <option >Map</option>
+              </select>
+            </li>
+            <li>Refurbished</li>
+            <li>Clothes</li>
+          </ul>
+        </div>
+        <div class="profile">
+          <?php include "includes/profile.php"; ?>
+        </div>
+      </nav>
+    </header>
     <main>
       <div class="container">
         <?php include "includes/card.php"; ?>
@@ -80,9 +110,6 @@
       function sessionKill(){
         <?php
           session_start();
-          session_unset();
-          session_destroy();
-          header("Location:../index.php");
         ?>
   }
     </script> -->
